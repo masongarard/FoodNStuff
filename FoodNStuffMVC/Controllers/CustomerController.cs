5756a565246a2697209ebea011e58dc5091e8eb4
masongarard <masongarard@gmail.com>
2019-06-25 14:06:10 -0400

    Created Post Method and View

---
+++ FoodNStuffMVC/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using FoodNStuffMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodNStuffMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+
+        //GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Post: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+
+    }
+}
--- FoodNStuffMVC/Models/Customer.cs
+++ FoodNStuffMVC/Models/Customer.cs
@@ -12,12 +12,15 @@ namespace FoodNStuffMVC.Models
         public int CustomerID { get; set; }
 
         [Required]
+        [Display(Name ="First Human Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name ="Last Human Name")]
         public string LastName { get; set; }
 
         //public string FullName => return $"{FirstName} {LastName}"; (<--THIS DOES THE SAME THING AS VVVV)
+        [Display(Name ="Full Human Name")]
         public string FullName
         {
             get
